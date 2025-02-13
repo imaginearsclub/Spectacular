02a184c802fc66ac11021502a18208a949afc810
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-02-13 02:24:06 -0500

    Refactor BlockAction to improve validation and encapsulation.
    Replaced direct field access with getter and setter methods to ensure better encapsulation and validation. Added state validation to prevent null location or material issues. Enhanced error handling and streamlined material/block data creation logic.

--- src/main/java/network/palace/show/actions/BlockAction.java
+++ src/main/java/network/palace/show/actions/BlockAction.java
@@ -2,8 +2,6 @@ package network.palace.show.actions;
 
 import network.palace.show.Show;
 import network.palace.show.exceptions.ShowParseException;
-import network.palace.show.handlers.BlockData;
-import network.palace.show.utils.ShowUtil;
 import network.palace.show.utils.WorldUtil;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,43 +9,75 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class BlockAction extends ShowAction {
+    private static final String VALIDATION_ERROR_MESSAGE = "Location and Material must not be null";
     public Location location;
     public Material blockMat;
 
-    public BlockAction(Show show, long time) {
+    public BlockAction(final Show show, final long time) {
         super(show, time);
     }
 
-    public BlockAction(Show show, long time, Location location, Material blockMat) {
-        super(show, time);
-        this.location = location;
-        this.blockMat = blockMat;
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public void play(Player[] nearPlayers) {
-        Block block = location.getBlock();
-        block.setType(blockMat);
+    public BlockAction(final Show show, final long time, final Location location, final Material blockMat) {
+        this(show, time);
+        setLocation(location);
+        setBlockMaterial(blockMat);
     }
 
     @Override
-    public ShowAction load(String line, String... args) throws ShowParseException {
-        Location loc = WorldUtil.strToLoc(show.getWorld().getName() + "," + args[3]);
-        if (loc == null) {
+    public void play(Player[] nearPlayers) {
+        validateState();
+        final Block block = location.getBlock();
+        block.setBlockData(blockMat.createBlockData());
+    }
+
+    @Override
+    public ShowAction load(final String line, final String... args) throws ShowParseException {
+        if (args.length < 4) {
+            throw new ShowParseException("Invalid BlockAction: " + line);
+        }
+        final Location parsedLocation = WorldUtil.strToLoc(show.getWorld().getName() + "," + args[3]);
+        if (parsedLocation == null) {
             throw new ShowParseException("Invalid Location " + line);
         }
         try {
-            this.blockMat = Material.valueOf(args[2].toUpperCase());
-            this.location = loc;
-        } catch (IllegalArgumentException e) {
-            throw new ShowParseException(e.getMessage());
+            setBlockMaterial(Material.valueOf(args[2].toUpperCase()));
+            setLocation(parsedLocation);
+        } catch (final IllegalArgumentException e) {
+            throw new ShowParseException("Cannot parse material: " + args[2] + " - " + e.getMessage());
         }
         return this;
     }
 
     @Override
-    protected ShowAction copy(Show show, long time) throws ShowParseException {
-        return new BlockAction(show, time, location, blockMat);
+    protected ShowAction copy(final Show show, final long time) throws ShowParseException {
+        return new BlockAction(show, time, getLocation(), getBlockMaterial());
+    }
+
+    public Location getLocation() {
+        return location.clone();
+    }
+
+    public void setLocation(final Location location) {
+        if (location == null) {
+            throw new IllegalArgumentException("Location cannot be null");
+        }
+        this.location = location.clone();
+    }
+
+    public Material getBlockMaterial() {
+        return blockMat;
+    }
+
+    public void setBlockMaterial(final Material blockMaterial) {
+        if (blockMaterial == null) {
+            throw new IllegalArgumentException("Material cannot be null");
+        }
+        this.blockMat = blockMaterial;
+    }
+
+    private void validateState() {
+        if (location == null || blockMat == null) {
+            throw new IllegalStateException(VALIDATION_ERROR_MESSAGE);
+        }
     }
 }
